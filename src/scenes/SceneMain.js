ad999e5370d9c297b61e0de04c3f758efe7ccc1d
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-18 19:03:30 +0530

    Add different sound and image

--- src/scenes/SceneMain.js
+++ src/scenes/SceneMain.js
@@ -5,31 +5,65 @@ class SceneMain extends Phaser.Scene {
   preload() {
   	this.load.image("sprBg0", "content/sprBg0.png");
   	this.load.image("sprBg1", "content/sprBg1.png");
-this.load.spritesheet("sprExplosion", "content/sprExplosion.png", {
-  frameWidth: 32,
-  frameHeight: 32
-});
-this.load.spritesheet("sprEnemy0", "content/sprEnemy0.png", {
-  frameWidth: 16,
-  frameHeight: 16
-});
-this.load.image("sprEnemy1", "content/sprEnemy1.png");
-this.load.spritesheet("sprEnemy2", "content/sprEnemy2.png", {
-  frameWidth: 16,
-  frameHeight: 16
-});
-this.load.image("sprLaserEnemy0", "content/sprLaserEnemy0.png");
-this.load.image("sprLaserPlayer", "content/sprLaserPlayer.png");
-this.load.spritesheet("sprPlayer", "content/sprPlayer.png", {
-  frameWidth: 16,
-  frameHeight: 16
-});
-this.load.audio("sndExplode0", "content/sndExplode0.wav");
-this.load.audio("sndExplode1", "content/sndExplode1.wav");
-this.load.audio("sndLaser", "content/sndLaser.wav");
+	this.load.spritesheet("sprExplosion", "content/sprExplosion.png", {
+	  frameWidth: 32,
+	  frameHeight: 32
+	});
+	this.load.spritesheet("sprEnemy0", "content/sprEnemy0.png", {
+	  frameWidth: 16,
+	  frameHeight: 16
+	});
+	this.load.image("sprEnemy1", "content/sprEnemy1.png");
+	this.load.spritesheet("sprEnemy2", "content/sprEnemy2.png", {
+	  frameWidth: 16,
+	  frameHeight: 16
+	});
+	this.load.image("sprLaserEnemy0", "content/sprLaserEnemy0.png");
+	this.load.image("sprLaserPlayer", "content/sprLaserPlayer.png");
+	this.load.spritesheet("sprPlayer", "content/sprPlayer.png", {
+	  frameWidth: 16,
+	  frameHeight: 16
+	});
+	this.load.audio("sndExplode0", "content/sndExplode0.wav");
+	this.load.audio("sndExplode1", "content/sndExplode1.wav");
+	this.load.audio("sndLaser", "content/sndLaser.wav");
   }
 
   create() {
-    
+    this.anims.create({
+      key: "sprEnemy0",
+      frames: this.anims.generateFrameNumbers("sprEnemy0"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "sprEnemy2",
+      frames: this.anims.generateFrameNumbers("sprEnemy2"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "sprExplosion",
+      frames: this.anims.generateFrameNumbers("sprExplosion"),
+      frameRate: 20,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: "sprPlayer",
+      frames: this.anims.generateFrameNumbers("sprPlayer"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.sfx = {
+  explosions: [
+    this.sound.add("sndExplode0"),
+    this.sound.add("sndExplode1")
+  ],
+  laser: this.sound.add("sndLaser")
+};
   }
 }
--- src/scenes/SceneMainMenu.js
+++ src/scenes/SceneMainMenu.js
@@ -3,6 +3,18 @@ class SceneMainMenu extends Phaser.Scene {
     super({ key: "SceneMainMenu" });
   }
 
+  preload(){
+  	this.load.image("sprBtnPlay", "content/sprBtnPlay.png");
+	this.load.image("sprBtnPlayHover", "content/sprBtnPlayHover.png");
+	this.load.image("sprBtnPlayDown", "content/sprBtnPlayDown.png");
+	this.load.image("sprBtnRestart", "content/sprBtnRestart.png");
+	this.load.image("sprBtnRestartHover", "content/sprBtnRestartHover.png");
+	this.load.image("sprBtnRestartDown", "content/sprBtnRestartDown.png");
+
+	this.load.audio("sndBtnOver", "content/sndBtnOver.wav");
+	this.load.audio("sndBtnDown", "content/sndBtnDown.wav");
+  }
+
   create() {
     this.scene.start("SceneMain");
   }
