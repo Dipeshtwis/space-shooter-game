dda08d65e6019a33855a86d53dd3a5d2f26def65
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-19 15:49:40 +0530

    Add frustum culling for removing off screen element

--- src/scenes/SceneMain.js
+++ src/scenes/SceneMain.js
@@ -153,34 +153,76 @@ class SceneMain extends Phaser.Scene {
   update(){
   	this.player.update();
 
-	if (this.keyW.isDown) {
-	  this.player.moveUp();
-	}
-	else if (this.keyS.isDown) {
-	  this.player.moveDown();
-	}
-  else if(this.keyB.isDown){
-    this.player.BoostUp();
-  }
+  	if (this.keyW.isDown) {
+  	  this.player.moveUp();
+  	}
+  	else if (this.keyS.isDown) {
+  	  this.player.moveDown();
+  	}
+    else if(this.keyB.isDown){
+      this.player.BoostUp();
+    }
 
-	if (this.keyA.isDown) {
-	  this.player.moveLeft();
-	}
-	else if (this.keyD.isDown) {
-	  this.player.moveRight();
-	}
-  if (this.keySpace.isDown) {
-  this.player.setData("isShooting", true);
-  }
-  else {
-    this.player.setData("timerShootTick", this.player.getData("timerShootDelay") - 1);
-    this.player.setData("isShooting", false);
-  }
+  	if (this.keyA.isDown) {
+  	  this.player.moveLeft();
+  	}
+  	else if (this.keyD.isDown) {
+  	  this.player.moveRight();
+  	}
+    if (this.keySpace.isDown) {
+    this.player.setData("isShooting", true);
+    }
+    else {
+      this.player.setData("timerShootTick", this.player.getData("timerShootDelay") - 1);
+      this.player.setData("isShooting", false);
+    }
 
-  for (var i = 0; i < this.enemies.getChildren().length; i++) {
-      var enemy = this.enemies.getChildren()[i];
+    for (let i = 0; i < this.enemies.getChildren().length; i++) {
+        var enemy = this.enemies.getChildren()[i];
 
-      enemy.update();
+        enemy.update();
+
+      if (enemy.x < -enemy.displayWidth ||
+        enemy.x > this.game.config.width + enemy.displayWidth ||
+        enemy.y < -enemy.displayHeight * 4 ||
+        enemy.y > this.game.config.height + enemy.displayHeight) {
+
+        if (enemy) {
+          if (enemy.onDestroy !== undefined) {
+            enemy.onDestroy();
+          }
+
+          enemy.destroy();
+        }
+      }
+    }
+
+    for (var i = 0; i < this.enemyLasers.getChildren().length; i++) {
+        var laser = this.enemyLasers.getChildren()[i];
+        laser.update();
+
+        if (laser.x < -laser.displayWidth ||
+          laser.x > this.game.config.width + laser.displayWidth ||
+          laser.y < -laser.displayHeight * 4 ||
+          laser.y > this.game.config.height + laser.displayHeight) {
+          if (laser) {
+            laser.destroy();
+          }
+        }
+      }
+
+    for (var i = 0; i < this.playerLasers.getChildren().length; i++) {
+      var laser = this.playerLasers.getChildren()[i];
+      laser.update();
+
+      if (laser.x < -laser.displayWidth ||
+        laser.x > this.game.config.width + laser.displayWidth ||
+        laser.y < -laser.displayHeight * 4 ||
+        laser.y > this.game.config.height + laser.displayHeight) {
+        if (laser) {
+          laser.destroy();
+        }
+      }
     }
   }
 
