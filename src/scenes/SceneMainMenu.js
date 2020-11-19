13cfda0f41f0ca34a4782c1c8b0fc1b625f44579
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-19 21:31:45 +0530

    Add menu title and background

--- src/scenes/SceneMainMenu.js
+++ src/scenes/SceneMainMenu.js
@@ -1,3 +1,5 @@
+import sprBg0 from "../content/sprBg0.png";
+import sprBg1 from "../content/sprBg1.png";
 import sndBtnOver from "../content/sndBtnOver.wav";
 import sndBtnDown from "../content/sndBtnDown.wav";
 import sprBtnPlay from "../content/sprBtnPlay.png";
@@ -7,12 +9,16 @@ import sprBtnRestart from "../content/sprBtnRestart.png";
 import sprBtnRestartHover from "../content/sprBtnRestartHover.png";
 import sprBtnRestartDown from "../content/sprBtnRestartDown.png";
 
+import ScrollingBackground from "../Layout/ScrollingBackground";
+
 class SceneMainMenu extends Phaser.Scene {
   constructor() {
     super({ key: "SceneMainMenu" });
   }
 
   preload(){
+    this.load.image("sprBg0", sprBg0);
+    this.load.image("sprBg1", sprBg1);
     this.load.image("sprBtnPlay", sprBtnPlay);
     this.load.image("sprBtnPlayHover", sprBtnPlayHover);
     this.load.image("sprBtnPlayDown", sprBtnPlayDown);
@@ -56,6 +62,29 @@ class SceneMainMenu extends Phaser.Scene {
       this.btnPlay.setTexture("sprBtnPlay");
       this.scene.start("SceneMain");
     }, this);
+
+    this.title = this.add.text(this.game.config.width * 0.5, 128, "SPACE SHOOTER", {
+      fontFamily: 'monospace',
+      fontSize: 48,
+      fontStyle: 'bold',
+      color: '#ffffff',
+      align: 'center'
+    });
+
+    this.title.setOrigin(0.5);
+
+    this.backgrounds = [];
+    for (var i = 0; i < 5; i++) {
+      var keys = ["sprBg0", "sprBg1"];
+      var key = keys[Phaser.Math.Between(0, keys.length - 1)];
+      var bg = new ScrollingBackground(this, key, i * 10);
+      this.backgrounds.push(bg);
+    }
+  }
+    update() {
+    for (var i = 0; i < this.backgrounds.length; i++) {
+      this.backgrounds[i].update();
+    }
   }
 }
 
