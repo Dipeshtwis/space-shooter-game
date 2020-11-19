bb5e4824b000f5e5acfc683d22c1b3455d203827
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-19 21:08:16 +0530

    Add main menu play button

--- src/scenes/SceneMainMenu.js
+++ src/scenes/SceneMainMenu.js
@@ -25,7 +25,37 @@ class SceneMainMenu extends Phaser.Scene {
   }
 
   create() {
-    this.scene.start("SceneMain");
+    this.sfx = {
+      btnOver: this.sound.add("sndBtnOver"),
+      btnDown: this.sound.add("sndBtnDown")
+    };
+
+    this.btnPlay = this.add.sprite(
+      this.game.config.width * 0.5,
+      this.game.config.height * 0.5,
+      "sprBtnPlay"
+    );
+
+    this.btnPlay.setInteractive();
+
+    this.btnPlay.on("pointerover", function() {
+      this.btnPlay.setTexture("sprBtnPlayHover"); // set the button texture to sprBtnPlayHover
+      this.sfx.btnOver.play(); // play the button over sound
+    }, this);
+
+    this.btnPlay.on("pointerout", function() {
+      this.setTexture("sprBtnPlay");
+    });
+
+    this.btnPlay.on("pointerdown", function() {
+      this.btnPlay.setTexture("sprBtnPlayDown");
+      this.sfx.btnDown.play();
+    }, this);
+
+    this.btnPlay.on("pointerup", function() {
+      this.btnPlay.setTexture("sprBtnPlay");
+      this.scene.start("SceneMain");
+    }, this);
   }
 }
 
